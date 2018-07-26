504054d27056cfa25cff589193c88aff62e18ce7
rqz <qiuzhang.rui@cpoao.com>
2018-07-26 17:50:52 +0800

    add some code

--- app/src/main/java/com/cloudpoint/plugins/ipc/simple/demo/GameActivity.java
+++ app/src/main/java/com/cloudpoint/plugins/ipc/simple/demo/GameActivity.java
@@ -3,16 +3,32 @@ package com.cloudpoint.plugins.ipc.simple.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.cloudpoint.plugins.ipc.simple.IIpcResponse;
 import com.cloudpoint.plugins.ipc.simple.IpcIntentAction;
+import com.cloudpoint.plugins.ipc.simple.IpcIntentProxy;
+import com.cloudpoint.plugins.ipc.simple.domain.GameEndState;
 import com.cloudpoint.plugins.ipc.simple.domain.GameStart;
 import com.cloudpoint.plugins.ipc.simple.domain.GameStartState;
 
 public class GameActivity extends AppCompatActivity {
 
+    IpcIntentProxy<GameEndState> gameStateIpcIntentProxy;
+
+    final IIpcResponse<GameEndState> gameStateIIpcResponse=new IIpcResponse<GameEndState>() {
+        @Override
+        public void onData(GameEndState gameState) {
+            //TODO :logical
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        gameStateIpcIntentProxy =new IpcIntentProxy<>(getApplicationContext(),GameEndState.class);
+        gameStateIpcIntentProxy.setCallback(gameStateIIpcResponse);
 
         GameStart gameStart=IpcIntentAction.get(getIntent(),GameStart.class);
 
@@ -20,5 +36,11 @@ public class GameActivity extends AppCompatActivity {
         GameStartState state =new GameStartState(gameStart.getOrderId(),gameStart.getGameId(),1,"ok",System.currentTimeMillis());
         state.send(getApplicationContext());
 
+
+
+
     }
+
+
+
 }
